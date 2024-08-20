2e762fbbe6144ffa765098efd970b341dc5615e8
JgTurizo <turizogregoriojose@outlook.com>
2024-08-20 09:38:42 -0500

    Ejercicio2.js

--- Ejercicio2.js
+++ Ejercicio2.js
@@ -9,41 +9,37 @@ const S = hashProcessor.getS();
  */
 function arrayCuadradosOrdenados(arr, S) {
     const n = arr.length;
-    const resultado = new Array(n).fill(0);
+    const resultado = new Array(n);
     let izquierda = 0;
     let derecha = n - 1;
-    let indice = n - 1;
     const rangoMaximo = S * 11;
 
-    while (izquierda <= derecha) {
+    for (let i = n - 1; i >= 0; i--) {
         const cuadradoIzquierda = arr[izquierda] * arr[izquierda];
         const cuadradoDerecha = arr[derecha] * arr[derecha];
 
         if (cuadradoIzquierda > cuadradoDerecha) {
-            if (cuadradoIzquierda <= rangoMaximo) {
-                resultado[indice] = cuadradoIzquierda;
-                indice--;
-            }
+            resultado[i] = cuadradoIzquierda;
             izquierda++;
         } else {
-            if (cuadradoDerecha <= rangoMaximo) {
-                resultado[indice] = cuadradoDerecha;
-                indice--;
-            }
+            resultado[i] = cuadradoDerecha;
             derecha--;
         }
     }
 
     // Eliminar valores que exceden el rango máximo
-    return resultado.filter(num => num !== 0);
+    return resultado.filter(num => num <= rangoMaximo);
 }
 
 // Ejemplos de uso
 const arr1 = [1, 2, 3, 5, 6, 8, 9];
-console.log(arrayCuadradosOrdenados(arr1, S)); // [1, 4, 9, 25, 36, 49, 64, 81]
+console.log(arrayCuadradosOrdenados(arr1, S)); // [1, 4, 9, 25, 36, 64]
 
 const arr2 = [-2, -1];
 console.log(arrayCuadradosOrdenados(arr2, S)); // [1, 4]
 
 const arr3 = [-6, -5, 0, 5, 6];
 console.log(arrayCuadradosOrdenados(arr3, S)); // [0, 25, 25, 36, 36]
+
+const arr4 = [-10, 10];
+console.log(arrayCuadradosOrdenados(arr4, S)); // []
